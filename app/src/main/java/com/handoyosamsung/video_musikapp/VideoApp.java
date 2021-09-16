93e5c925f446b3a0cc5ecabdcc098c58dd039ace
Itsuka22 <111201911829@mhs.dinus.ac.id>
2021-09-16 09:12:08 +0700

    first commit

---
+++ app/src/main/java/com/handoyosamsung/video_musikapp/VideoApp.java
@@ -0,0 +1,49 @@
+package com.handoyosamsung.video_musikapp;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class VideoApp extends AppCompatActivity {
+
+    //Deklarasi Variable
+    private VideoView videoView;
+    private MediaController mediaController;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Inisialisasi VideoView, MediaController dan Button
+        videoView = findViewById(R.id.video);
+        Button playVideo = findViewById(R.id.playing);
+        mediaController = new MediaController(this);
+
+
+        //Menjalankan Video saat tombol Play di Klik
+        playVideo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //Variable Uri untuk menentukan lokasi Resource Video yang akan ditampilkan
+                Uri uri = Uri.parse("android.resource://"+getPackageName()+"/"+R.raw.tsukihime_video);
+
+                videoView.setVideoURI(uri);
+
+                //Memasang MediaController untuk menampilkan tombol play, pause, dsb
+                videoView.setMediaController(mediaController);
+                mediaController.setAnchorView(videoView);
+
+                //Menjalankan Video
+                videoView.start();
+            }
+        });
+    }
+}
